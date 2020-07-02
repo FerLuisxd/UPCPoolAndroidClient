857a8c89d75d45bc05031d5246262cb63a48643d
Rodrigo Lozano <rolozanocampos@hotmail.com>
2020-07-02 13:04:40 -0500

    Commit por si acaso

--- app/src/main/java/com/example/upcpool/controllers/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/upcpool/controllers/fragments/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.example.upcpool.controllers.fragments
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,24 +12,18 @@ import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.example.upcpool.R
 import com.example.upcpool.controllers.activities.RoomActivity
 import com.example.upcpool.database.RoomDB
 import com.example.upcpool.entity.UserDto
-import com.example.upcpool.models.Availables
 import com.example.upcpool.models.Reservation
-import com.example.upcpool.models.Seat
 import com.example.upcpool.network.RoomService
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -117,26 +110,6 @@ class HomeFragment : Fragment(){
                         view.findViewById<Button>(R.id.btn_room).visibility = Button.INVISIBLE
                         view.findViewById<ConstraintLayout>(R.id.actual_reserve).visibility = ConstraintLayout.VISIBLE
 
-                        if (reservation.active) {
-
-                            view.findViewById<TextView>(R.id.tv_actualDate).text= date
-                            view.findViewById<TextView>(R.id.tv_actualOffice).text= reservation.room.office
-                            view.findViewById<TextView>(R.id.tv_actualRecurso1).text = reservation.room.features[0]
-                            view.findViewById<TextView>(R.id.tv_actuLRecurso2).text = reservation.room.features[1]
-                            view.findViewById<TextView>(R.id.tv_actualCode).text = reservation.room.code
-                            view.findViewById<TextView>(R.id.tv_actualSeats).text = reservation.room.seats.toString()
-
-                            view.findViewById<Button>(R.id.btn_shared).isEnabled = false
-                            view.findViewById<Button>(R.id.btn_shared).visibility = Button.INVISIBLE
-                            view.findViewById<ConstraintLayout>(R.id.actual_room).visibility = ConstraintLayout.VISIBLE
-
-                            val layout = view.findViewById<ConstraintLayout>(R.id.actual_room)
-                            layout.setOnClickListener{
-                                val intento = Intent(context, RoomActivity::class.java)
-                                intento.putExtra("Room", reservation.room)
-                                startActivity(intento)
-                            }
-                        }
                         val layout = view.findViewById<ConstraintLayout>(R.id.actual_reserve)
                         layout.setOnClickListener{
 
@@ -177,8 +150,8 @@ class HomeFragment : Fragment(){
                                     }
                                 })*/
                         }
-
                     }
+                    entrar(view)
                 }
 
                 else{
@@ -278,8 +251,88 @@ class HomeFragment : Fragment(){
 
     }
 
-    private fun entrar(){
+    private fun entrar(view: View){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://upc-pool-ferluisxd.cloud.okteto.net/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        val roomService: RoomService
+        roomService = retrofit.create(RoomService::class.java)
+        val request = roomService.getPublics(getHeaderMap(token))
+        request.enqueue(object : Callback<List<Reservation>> {
+            override fun onFailure(call: Call<List<Reservation>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(
+                call: Call<List<Reservation>>,
+                responseDetails: Response<List<Reservation>>
+            ) {
+                Log.d("MSG", responseDetails.message())
+                Log.d("Error Body", responseDetails.errorBody().toString())
+                Log.d("Activity Success", responseDetails.raw().toString())
+                Log.d("Activity Success", responseDetails.body().toString())
+
+                val listPubs = responseDetails.body()
+
+                val r = roomService.getUser(getHeaderMap(token))
+                r.enqueue(object : Callback<UserDto>{
+                    override fun onFailure(call: Call<UserDto>, t: Throwable) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
+                        Log.d("MSG", response.message())
+                        Log.d("Error Body", response.errorBody().toString())
+                        Log.d("Activity Success", response.raw().toString())
+                        Log.d("Activity Success", response.body().toString())
+
+                        val auxUser = response.body() ?: null
+
+                        if (listPubs != null) {
+                            listPubs.forEach {pub ->
+                                pub.seats.forEach {seat ->
+                                    if (auxUser != null) {
+                                        if (seat.name == auxUser.name) {
+                                            pub.room.theme = pub.theme
+                                            pub.room.pubId = pub._id
+
+                                            val cal = Calendar.getInstance()
+                                            cal.setTime(pub.start)
+                                            cal.add(Calendar.HOUR, -5)
+
+                                            val formatter = SimpleDateFormat("dd-MMMM HH:00");
+
+                                            val date = formatter.format(cal.time)
+
+                                            view.findViewById<TextView>(R.id.tv_actualDate).text= date
+                                            view.findViewById<TextView>(R.id.tv_actualOffice).text= pub.room.office
+                                            view.findViewById<TextView>(R.id.tv_actualRecurso1).text = pub.room.features[0]
+                                            view.findViewById<TextView>(R.id.tv_actuLRecurso2).text = pub.room.features[1]
+                                            view.findViewById<TextView>(R.id.tv_actualCode).text = pub.room.code
+                                            view.findViewById<TextView>(R.id.tv_actualSeats).text = pub.room.seats.toString()
+
+                                            view.findViewById<Button>(R.id.btn_shared).isEnabled = false
+                                            view.findViewById<Button>(R.id.btn_shared).visibility = Button.INVISIBLE
+                                            view.findViewById<ConstraintLayout>(R.id.actual_room).visibility = ConstraintLayout.VISIBLE
+
+                                            val layout = view.findViewById<ConstraintLayout>(R.id.actual_room)
+                                            layout.setOnClickListener{
+                                                val intento = Intent(context, RoomActivity::class.java)
+                                                intento.putExtra("Room", pub.room)
+                                                startActivity(intento)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+            }
+
+        })
     }
 
     /*override fun onItemClicked(room: Room) {
